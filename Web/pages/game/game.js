1c24124d3b66cbb031de8d1a21967f284fa7d4e1
Spurt-Works <88038048+Spurt-Works@users.noreply.github.com>
2023-11-03 19:09:01 -0300

    Update game.js

--- Web/pages/game/game.js
+++ Web/pages/game/game.js
@@ -1,1 +1,20 @@
 const canvas = document.getElementById("gameCanvas");
+
+// Get a reference to the basket element
+const basket = document.getElementById('basket');
+
+/*
+  This function moves the basket when the user moves their mouse.
+
+   Author: Emmet
+*/
+function moveBasket(){
+  // Add an event listener to track mouse movement
+  document.addEventListener('mousemove', (event) => {
+    // Get the X-coordinate of the mouse pointer
+    const mouseX = event.clientX;
+
+    // Update the basket's left position to follow the mouse
+    basket.style.left = mouseX + 'px';
+  });
+}
